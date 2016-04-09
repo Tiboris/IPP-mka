3a708c813271c39bd5a8bd7818f112ea20a8e93c
Tiboris <xdudla00@vutbr.cz>
2016-04-10 01:30:26 +0200

    Issue with help and wrong argumenst

--- mka.py
+++ mka.py
@@ -2,34 +2,62 @@
 #-*- coding: utf-8 -*-
 #MKA:xdudla00
 #------------------------------------------------------------------------------
-import re 							# to searching by regex
-import os 							# to work with filesystem
-import sys							# to get arguments
+import re                           # to searching by regex
+import os                           # to work with filesystem
+import sys                          # to get arguments
 import argparse
 #------------------------------FUNCTIONS---------------------------------------
-def check_args(argv):
-	parser = argparse.ArgumentParser()
-	parser.add_argument('--foo', help='foo help')
-	args = parser.parse_args()
-	return args
-
-def do_something(a):
-	print(a)
-
+#------------------------------------------------------------------------------
+def check_args(): # TODO fix duplicated params
+    parser = argparse.ArgumentParser(add_help=False, 
+                description='Script for processing finite state machines')
+    parser.add_argument('--help', #help='show this help message and exit', 
+                required=False, action="store_true")
+    parser.add_argument('--input', required=False,
+                #help='if not set it takes STDIN', 
+                default=sys.stdin)
+    parser.add_argument('--output', required=False,
+                #help='if not set output will be at STDOUT', 
+                default=sys.stdout)
+    parser.add_argument('-m','--minimize', required=False,
+                #help='minimizes finite state machine', 
+                action="store_true")
+    parser.add_argument('-f','--find-non-finishing', required=False,
+                #help='find states and prints them otherwise \'0\'',
+                action="store_true")
+    parser.add_argument('-i','--case-insensitive', required=False, 
+                #help='ignoring input states strings case', 
+                action="store_true")
+    try :
+        args = parser.parse_args()
+    except :
+        exit(9)
+    # if args.help : 
+    #     print(parser.format_help())
+    return args
+#------------------------------------------------------------------------------
+def read_input(input_file):
+    if input_file != sys.stdin :
+        try :
+            input_file=open(input_file,'r')
+        except :
+            print_err("Can not open file",9)
+    for line in input_file:
+        print(line)
+#------------------------------------------------------------------------------
+def print_err(msg,code):
+    print(msg)
+    exit(code)
+#------------------------------------------------------------------------------
 #-----------------------------MAIN-FUNCTION------------------------------------
-def main(argv):
-
-	if ( check_args(argv) ):
-		do_something("asdf")
-	else:
-		print("Invalid option or their combination")		
+def main():
+    args = check_args()
+    read_input(args.input)
+    return 0
 #------------------------------------------------------------------------------
 if __name__ == "__main__":
-    main(sys.argv)	
+    exit(main())    
 #------------------------------------------------------------------------------
-#help
-#input
-#output
 
 # -f, --find-non-finishing -> hledá neukončující stav zadaného dobře specifikovaného
 # konečného automatu (automat se nevypisuje). Nalezne-li jej, bez odřádkování jej vypíše na
