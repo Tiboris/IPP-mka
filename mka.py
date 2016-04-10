5fb270da5e6a42250350ae6885475a832ac48c01
Tiboris <xdudla00@vutbr.cz>
2016-04-10 02:31:01 +0200

    Some improovements in parsing

--- mka.py
+++ mka.py
@@ -5,43 +5,52 @@
 import re                           # to searching by regex
 import os                           # to work with filesystem
 import sys                          # to get arguments
-import argparse
+import argparse                     # to parse arguments
+#------------------------------------------------------------------------------
+ARGS_ERR = 1
+READ_ERR = 2
+WRIT_ERR = 3
+FORM_ERR = 60
+SEMS_ERR = 61
+DSKA_ERR = 62
 #------------------------------FUNCTIONS---------------------------------------
 #------------------------------------------------------------------------------
 def check_args(): # TODO fix duplicated params
     parser = argparse.ArgumentParser(add_help=False, 
                 description='Script for processing finite state machines')
-    parser.add_argument('--help', #help='show this help message and exit', 
+    parser.add_argument('--help', help='show this help message and exit', 
                 required=False, action="store_true")
     parser.add_argument('--input', required=False,
-                #help='if not set it takes STDIN', 
+                help='if not set it takes STDIN', 
                 default=sys.stdin)
     parser.add_argument('--output', required=False,
-                #help='if not set output will be at STDOUT', 
+                help='if not set output will be at STDOUT', 
                 default=sys.stdout)
     parser.add_argument('-m','--minimize', required=False,
-                #help='minimizes finite state machine', 
+                help='minimizes finite state machine', 
                 action="store_true")
     parser.add_argument('-f','--find-non-finishing', required=False,
-                #help='find states and prints them otherwise \'0\'',
+                help='find states and prints them otherwise \'0\'',
                 action="store_true")
     parser.add_argument('-i','--case-insensitive', required=False, 
-                #help='ignoring input states strings case', 
+                help='ignoring input states strings case', 
                 action="store_true")
-    try :
+    try :       # not working still on stderr
         args = parser.parse_args()
     except :
-        exit(9)
-    # if args.help : 
-    #     print(parser.format_help())
+        print_err("",ARGS_ERR)
+    if args.find_non_finishing == args.minimize and args.minimize == True :
+        print_err("Wrong combination of arguments",ARGS_ERR)
+    if args.help : 
+        print(parser.format_help())
     return args
 #------------------------------------------------------------------------------
 def read_input(input_file):
     if input_file != sys.stdin :
-        try :
+        try:
             input_file=open(input_file,'r')
-        except :
-            print_err("Can not open file",9)
+        except:
+            print_err("Can not open file",READ_ERR)
     for line in input_file:
         print(line)
 #------------------------------------------------------------------------------
